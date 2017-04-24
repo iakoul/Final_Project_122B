1ca59de824a284e7d09974be6affc2a9af385704
Alex Bustelo <abustelo@uci.edu>
2017-04-23 18:23:11 -0700

    Basic functionality of Browse complete, need to add support for browsing by number and non-alphanumeric characters.

--- CS122BWebProject/src/main/Browse.java
+++ CS122BWebProject/src/main/Browse.java
@@ -44,48 +44,127 @@ public class Browse extends HttpServlet {
 	    		+ request.getParameter("letter")
 	    		+ "</title>"
 	    		+ "</head>");
-		if ((Boolean)session.getAttribute("loggedIn")) {
-			//Incorporate mySQL driver
-			try {
-				Class.forName("com.mysql.jdbc.Driver").newInstance();
-			} catch (final Exception e) {
-				out.println("mySQL driver was not loaded");
-				out.println(e.getMessage());
-			}
-			
-			//connect to the database
-			try {
-				connection = DriverManager.getConnection("jdbc:mysql:///storemarketing?autoReconnect=true&useSSL=false","root","mysqlpass");
+	    out.println("<body bgcolor=\"#FDF5E6\">\n");
+	    try {
+	    	if ((Boolean)session.getAttribute("loggedIn")) {
+				//Incorporate mySQL driver
 				try {
-					
-					String prepQuery = "SELECT * FROM UsersTbl u WHERE u.username = ?"; //change
-					PreparedStatement pstmt = connection.prepareStatement(prepQuery);
-					pstmt.setString(1, request.getParameter("letter")); //probably change
-					ResultSet results = pstmt.executeQuery();
-					/*
-					if (results.next()) {
-						if (BCrypt.checkpw(password, results.getString(3))) {
-							out.println("<h1 align=\"center\">Welcome, " + results.getString(1));
-							loggedIn = true;
-							if (results.getBoolean(2)) {
-								out.println("(A)");
-								session.setAttribute("isAdmin", true);
-							}
-							out.println("</h1>\n");
-						}
-					} else {
-						out.println("<h1>Incorrect username or password</h1>\n");
-						out.println(password);
-						loggedIn = false;
-					}
-					*/
-				} catch (SQLException e) {
-					out.println("Select statement failed with code " + e.getMessage());
+					Class.forName("com.mysql.jdbc.Driver").newInstance();
+				} catch (final Exception e) {
+					out.println("mySQL driver was not loaded");
+					out.println(e.getMessage());
 				}
-			} catch (SQLException e) {
-				out.println("Connection failed " + e.getMessage());
+				
+				//connect to the database
+				try {
+					connection = DriverManager.getConnection("jdbc:mysql:///storemarketing?autoReconnect=true&useSSL=false","root","mysqlpass");
+					try {
+						Integer storeCount = 0;
+						String countQuery = "SELECT DISTINCT "
+								+ "COUNT(s.storeID)"
+								+ "FROM "
+								+ "`AcceptsPaymentTbl` ap, "
+								+ "`LanguageTbl` l1, "
+								+ "`LanguageTbl` l2, "
+								+ "`CityTbl` c, "
+								+ "`StoreTbl` s, "
+								+ "`PlazaTbl` p, "
+								+ "`OwnerTbl` o "
+								+ "WHERE "
+								+ "(s.storeName LIKE ?) AND "
+								+ "s.storeID = ap.storeID "
+								+ "AND s.plazaID = p.plazaID "
+								+ "AND p.cityID = c.cityID "
+								+ "AND s.ownerID = o.ownerID "
+								+ "AND o.primaryLangID = l1.langID "
+								+ "AND o.secondaryLangID = l2.langID;";
+						PreparedStatement pstmtCount = connection.prepareStatement(countQuery);
+						pstmtCount.setString(1, request.getParameter("letter") + "%");
+						ResultSet resultCount = pstmtCount.executeQuery();
+						if (resultCount.next()) {
+							storeCount = resultCount.getInt(1);
+						}
+						
+						out.println("<h1 align=\"center\">Browsing stores starting with letter " + request.getParameter("letter") + "</h1>\n");
+						out.println("<h1 align=\"center\">There are a total of " + storeCount.toString() + " starting with that letter</h1>\n");
+						
+						out.println("<table style=\"text-align: left; width: 100%; border: 1px solid black;\">\n");
+						out.println("<tr>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">Business Name</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">City</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">Visa</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">MasterCard</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">Discover</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">AmEx</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">Paypal</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">Venmo</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">Primary Language</th>\n");
+						out.println("<th style=\"border: 1px solid black; text-align: left;\">Secondary Language</th>\n");
+						out.println("</tr>");
+						
+						if (storeCount > 0) {
+							Integer limit = 10;
+							String prepQuery = "SELECT DISTINCT "
+									+ "s.storeName, "
+									+ "c.cityID, "
+									+ "ap.acceptsVisa, "
+									+ "ap.acceptsMasterCard, "
+									+ "ap.acceptsDiscover, "
+									+ "ap.acceptsAmEx, "
+									+ "ap.acceptsPaypal, "
+									+ "ap.acceptsVenmo, "
+									+ "l1.languageSpoken AS primaryLang, "
+									+ "l2.languageSpoken AS secondaryLang "
+									+ "FROM "
+									+ "`AcceptsPaymentTbl` ap, "
+									+ "`LanguageTbl` l1, "
+									+ "`LanguageTbl` l2, "
+									+ "`CityTbl` c, "
+									+ "`StoreTbl` s, "
+									+ "`PlazaTbl` p, "
+									+ "`OwnerTbl` o "
+									+ "WHERE "
+									+ "(s.storeName LIKE ?) AND "
+									+ "s.storeID = ap.storeID "
+									+ "AND s.plazaID = p.plazaID "
+									+ "AND p.cityID = c.cityID "
+									+ "AND s.ownerID = o.ownerID "
+									+ "AND o.primaryLangID = l1.langID "
+									+ "AND o.secondaryLangID = l2.langID "
+									+ "ORDER BY s.storeName "
+									+ "LIMIT ? OFFSET ?;";
+							PreparedStatement pstmt = connection.prepareStatement(prepQuery);
+							pstmt.setString(1, request.getParameter("letter")  + "%"); //probably change
+							pstmt.setInt(2, limit);
+							pstmt.setInt(3, (Integer.parseInt(request.getParameter("pg")) - 1) * limit);
+							ResultSet results = pstmt.executeQuery();
+							while (results.next()) {
+								out.println("<tr>\n");
+								out.println("<td>" + results.getString(1) + "</td>\n");
+								out.println("<td>" + results.getString(2) + "</td>\n");
+								out.println("<td>" + results.getBoolean(3) + "</td>\n");
+								out.println("<td>" + results.getBoolean(4) + "</td>\n");
+								out.println("<td>" + results.getBoolean(5) + "</td>\n");
+								out.println("<td>" + results.getBoolean(6) + "</td>\n");
+								out.println("<td>" + results.getBoolean(7) + "</td>\n");
+								out.println("<td>" + results.getBoolean(8) + "</td>\n");
+								out.println("<td>" + results.getString(9) + "</td>\n");
+								out.println("<td>" + results.getString(10) + "</td>\n");
+								out.println("</tr>\n");
+							}
+							out.println("<a href=\"./Browse?letter=" + request.getParameter("letter") + "&pg=" + (Integer.parseInt(request.getParameter("pg")) - 1) + "\">Back</a>");
+							out.println("<a href=\"./Browse?letter=" + request.getParameter("letter") + "&pg=" + (Integer.parseInt(request.getParameter("pg")) + 1) + "\">Next</a>");
+						}
+					} catch (SQLException e) {
+						out.println("Select statement failed with code " + e.getMessage());
+					}
+				} catch (SQLException e) {
+					out.println("Connection failed " + e.getMessage());
+				}
 			}
-		}
+	    } catch (NullPointerException e){
+	    	response.setHeader("Refresh", "3; URL=./index.html");
+	    }
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
