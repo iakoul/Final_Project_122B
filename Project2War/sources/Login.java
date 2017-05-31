01f14547eed6dc2427648077fd65d464037705cc
tinagitsit <tinal7@uci.edu>
2017-05-30 21:09:36 -0700

    Updated files for android app.

--- Project2War/sources/Login.java
+++ Project2War/sources/Login.java
@@ -3,6 +3,8 @@ import java.io.PrintWriter;
 // import java.math.BigDecimal;
 // import java.util.List;
 // import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.RequestDispatcher;
 import java.sql.*;
+import com.google.gson.*;
 
 /*
  * Checks login info
@@ -18,49 +21,57 @@ import java.sql.*;
 
 public class Login extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
-		
-		// boolean valid = VerifyUtils.verify(gRecaptchaResponse);
-		// if (!valid) {
-		// 	request.setAttribute("error", "Invalid reCaptcha. Please try again.");
-		// 	RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
-		// 	rd.forward(request, response);
-		// 	return;
-		// }
-
 		Connection connection = null;
 		PreparedStatement statement = null;
 		ResultSet result = null;
-
-		HttpSession session = request.getSession(true);
-		Boolean loggedIn = (Boolean)session.getAttribute("loggedIn");
-
 		PrintWriter out = response.getWriter();
 
-		if(loggedIn == null || loggedIn == false) {
-			try {
-			// Database connection info
-			String user = "mytestuser";
-        	String pass = "mypassword";
-        	String url = "jdbc:mysql://localhost:3306/storemarketing";
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		boolean isAndroid = SearchResult.isStringEmpty(request.getParameter("isAndroid")) ? false : Boolean.parseBoolean(request.getParameter("isAndroid"));
 
-			// Load driver
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		// redirect to main page if logged into browser
+		HttpSession session = request.getSession(true);
+		Boolean loggedIn = (Boolean)session.getAttribute("loggedIn");
+		
+		if(!isAndroid) { // check if logged into browser
+			String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
+			boolean valid = VerifyUtils.verify(gRecaptchaResponse);
+			if (!valid) { // verify recaptcha is correct
+				request.setAttribute("error", "Invalid reCaptcha. Please try again.");
+				RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
+				rd.forward(request, response);
+				return;
+			}
 
-			// Connect to mySQL
-			connection = DriverManager.getConnection(url, user, pass);
+			if(loggedIn != null && loggedIn == true) {
+				response.sendRedirect("Main.jsp");
+				return;
+			}
+		}
 
-			String loginQuery = "SELECT u.firstName FROM UsersTbl u WHERE u.username = ? AND u.hashPW = ? ";
-			statement = connection.prepareStatement(loginQuery);
-			statement.setString(1, username);
-			statement.setString(2, password);
-			result = statement.executeQuery();
+		try {
+		// Database connection info
+		String user = "mytestuser";
+    	String pass = "mypassword";
+    	String url = "jdbc:mysql://localhost:3306/storemarketing";
 
-			if(result.next()) {
-				String firstName = result.getString("u.firstName");
+		// Load driver
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+		// Connect to mySQL
+		connection = DriverManager.getConnection(url, user, pass);
+
+		String loginQuery = "SELECT u.firstName FROM UsersTbl u WHERE u.username = ? AND u.hashPW = ? ";
+		statement = connection.prepareStatement(loginQuery);
+		statement.setString(1, username);
+		statement.setString(2, password);
+		result = statement.executeQuery();
+
+		if(result.next()) {
+			String firstName = result.getString("u.firstName");
+			if(!isAndroid) {
 				session.setAttribute("name", firstName);
 				session.setAttribute("loggedIn", new Boolean(true));
 				session.setAttribute("username", username);
@@ -69,41 +80,60 @@ public class Login extends HttpServlet {
 				rd.forward(request, response);
 			}
 			else {
+				Map<String, String> map = new HashMap<String, String>();
+				map.put("loggedIn", "true");
+				map.put("name", firstName);
+				String json = new Gson().toJson(map);
+			    response.setContentType("application/json");
+			    response.setCharacterEncoding("UTF-8");
+			    response.getWriter().write(json);
+
+				// out.println("true");
+			}
+		}
+		else {
+			if(!isAndroid) {
 				// TODO: change to give error message with ajax
 				// response.sendRedirect("Login.jsp");
 				request.setAttribute("error", "Invalid username password combination. Please try again.");
 				RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
 				rd.forward(request, response);
 			}
+			else {
+				Map<String, String> map = new HashMap<String, String>();
+				map.put("loggedIn", "false");
+				String json = new Gson().toJson(map);
+			    response.setContentType("application/json");
+			    response.setCharacterEncoding("UTF-8");
+			    response.getWriter().write(json);
+				// out.println("false");
+			}
+		}
 
+		}
+		catch(Exception e) {
+			out.println(e.getMessage());
+		}
+		finally {
+			try {
+				result.close();
 			}
 			catch(Exception e) {
-				out.println(e.getMessage());
+				// ignore
 			}
-			finally {
-				try {
-					result.close();
-				}
-				catch(Exception e) {
-					// ignore
-				}
-				try {
-					statement.close();
-				}
-				catch(Exception e) {
-					// ignore
-				}
-				try {
-					connection.close();
-				}
-				catch(Exception e) {
-					// ignore
-				}
+			try {
+				statement.close();
 			}
-		}
-		else {
-			response.sendRedirect("Main.jsp");
-		}
+			catch(Exception e) {
+				// ignore
+			}
+			try {
+				connection.close();
+			}
+			catch(Exception e) {
+				// ignore
+			}
+		}		
 	}
 
 
